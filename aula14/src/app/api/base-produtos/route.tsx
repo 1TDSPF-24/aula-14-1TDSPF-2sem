d153a4465c33a6cf3952822d9ce0fade0028557a
Julio-CRodrigues <juliocesarccr29@gmail.com>
2024-09-25 12:01:37 -0300

    Realizando requisicao em api local

--- aula14/src/app/api/base-produtos/route.tsx
+++ aula14/src/app/api/base-produtos/route.tsx
@@ -1,6 +1,16 @@
 import { NextResponse } from "next/server";
+import { promises as fs } from "fs";
+
+
 
 // Método get-all-produtos
 export async function GET() {
-   return NextResponse.json({mensagem:"hellw World!"});
+
+    const file = await fs.readFile(process.cwd() + "/src/data/base.json", "utf-8");
+
+    //Realizar um parse do arquivo para json
+    const produtos = JSON.parse(file);
+
+    // Retornar o arquivo em formato de json para quem fez a Requisicao
+   return NextResponse.json(produtos);
 }
