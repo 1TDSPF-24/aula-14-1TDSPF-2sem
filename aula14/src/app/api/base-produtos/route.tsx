7499fafce8afca8232d28de7abe4f079c5568097
cadenasza <leonardocadena2005@gmail.com>
2024-09-25 12:00:18 -0300

    utilizando o fs e o cwd para pegar o arquivo base.json

--- aula14/src/app/api/base-produtos/route.tsx
+++ aula14/src/app/api/base-produtos/route.tsx
@@ -1,8 +1,17 @@
-// criando o metodo get - deve ter export, metodo async e ser uma funcao com nome GET sem paramentros, para assim retornar todos os produtos
-
 import { NextResponse } from "next/server";
+import { promises as fs } from "fs";
 
+
+// criando o metodo get all - deve ter export, metodo async e ser uma funcao com nome GET sem paramentros, para assim retornar todos os produtos
 export async function GET() {
-    return NextResponse.json({mensagem:"Hello World!"});
+
+    const file = await fs.readFile(process.cwd() + "/src/data/base.json", "utf-8");
+
+    //Realiar um parse do arquivo para JSON
+    const produtos = JSON.parse(file);
+
+
+    //Retornando o arquivo em formato de JSON para quem fez a requisicao:
+    return NextResponse.json(produtos);
 }
 
