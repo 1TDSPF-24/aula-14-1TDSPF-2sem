7312ee56dd2065c54aa1f68de3072e55c7ddf587
nathan <nathan.consolo17@gmail.com>
2024-09-25 12:03:58 -0300

    Trabalhando com API e com insominia

--- aula14/src/app/api/base-produtos/route.tsx
+++ aula14/src/app/api/base-produtos/route.tsx
@@ -1,8 +1,16 @@
 import { NextResponse } from "next/server";
+import { promises as hit } from "fs";
+
+
 
 export async function GET()
 {
-    return NextResponse.json({mensagem: "Hello World!"});
+    // process.cwd() retorna a pasta raiz do nosso projeto
+    const file = await hit.readFile(process.cwd() + "/src/data/base.json",
+    "utf8");
 
-    
+    const produtos = JSON.parse(file);
+
+    // Retorna o arquivo em formato JSON para quem fez a requisição
+    return NextResponse.json({produtos});
 }
