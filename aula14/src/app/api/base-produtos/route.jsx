c22e14dd482d6886e6c638af6d2310e9621c4d76
Adriano <presuntinhogg@gmail.com>
2024-09-25 12:03:50 -0300

    Realizando requisiçao em API local com Routers

--- aula14/src/app/api/base-produtos/route.jsx
+++ aula14/src/app/api/base-produtos/route.jsx
@@ -1,15 +1,14 @@
 import {NextResponse} from 'next/server'
+import {promises as fs } from 'fs'
 
-const lista = [
-    { id: 1, nome: "garfo", qtd: 1000 },
-    { id: 2, nome: "colher", qtd: 2000 },
-    { id: 3, nome: "faca", qtd: 1000 },
-    { id: 4, nome: "prato", qtd: 1000 },
-    { id: 5, nome: "copo", qtd: 1000 },
-    { id: 6, nome: "guardanapo", qtd: 10000 },
 
     export async function GET () {
-        return NextResponse.json(dados);
+
+        cons file = fs.readFile(process.cwd() + '/src/data/base.json,'utf-8');
+
+        const produtos = JSON.parse(file)
+
+        return NextResponse.json(produtos);
         
     }
-];
+{};
