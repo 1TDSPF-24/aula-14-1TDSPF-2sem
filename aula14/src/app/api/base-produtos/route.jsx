584e12ffe214063dd0ea0827e380b350935cea81
Adriano <presuntinhogg@gmail.com>
2024-09-25 11:49:19 -0300

    lista

--- aula14/src/app/api/base-produtos/route.jsx
+++ aula14/src/app/api/base-produtos/route.jsx
@@ -1,4 +1,15 @@
+import {NextResponse} from 'next/server'
 
-export async function GET(params:type){
-    return NextResponse.json({mensagem:"Hellow World"})
-}
+const lista = [
+    { id: 1, nome: "garfo", qtd: 1000 },
+    { id: 2, nome: "colher", qtd: 2000 },
+    { id: 3, nome: "faca", qtd: 1000 },
+    { id: 4, nome: "prato", qtd: 1000 },
+    { id: 5, nome: "copo", qtd: 1000 },
+    { id: 6, nome: "guardanapo", qtd: 10000 },
+
+    export async function GET () {
+        return NextResponse.json(dados);
+        
+    }
+];
